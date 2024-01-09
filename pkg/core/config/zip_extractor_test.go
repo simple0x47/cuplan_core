510bc248cea8f225b1b69f46aadbb957e6b2a0ac
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2024-01-10 00:50:45 +0100

    r use `z` as receiver for `ZipExtractorTestSuite`

--- pkg/core/config/zip_extractor_test.go
+++ pkg/core/config/zip_extractor_test.go
@@ -16,33 +16,33 @@ type ZipExtractorTestSuite struct {
 	TestDataPath string
 }
 
-func (s *ZipExtractorTestSuite) SetupTest() {
+func (z *ZipExtractorTestSuite) SetupTest() {
 	_, testFile, _, _ := runtime.Caller(0)
-	s.TestDataPath = core.GetTestDataPath(testFile).Unwrap()
+	z.TestDataPath = core.GetTestDataPath(testFile).Unwrap()
 }
 
-func (s *ZipExtractorTestSuite) TestZipExtractor_Extract_ValidZip_ExtractsExpectedFiles() {
+func (z *ZipExtractorTestSuite) TestZipExtractor_Extract_ValidZip_ExtractsExpectedFiles() {
 	extractor := NewZipExtractor()
 	uuid := uuid.New().String()
-	packageData, err := os.ReadFile(fmt.Sprintf("%sdummy.zip", s.TestDataPath))
+	packageData, err := os.ReadFile(fmt.Sprintf("%sdummy.zip", z.TestDataPath))
 	if err != nil {
-		assert.Fail(s.T(), fmt.Sprintf("failed to read 'dummy.zip': %s", err))
+		assert.Fail(z.T(), fmt.Sprintf("failed to read 'dummy.zip': %z", err))
 	}
 
 	result := extractor.Extract(packageData, uuid)
 
-	testDataPathResult := doesDirectoryExist(s.TestDataPath)
-	_, executableErr := os.Stat(fmt.Sprintf("%s/cp-config", uuid))
-	_, configFileErr := os.Stat(fmt.Sprintf("%s/config/config.yaml", uuid))
-	_, logConfigFileErr := os.Stat(fmt.Sprintf("%s/config/log4rs.yaml", uuid))
-	_, anotherFileErr := os.Stat(fmt.Sprintf("%s/config/subfolder/another.yaml", uuid))
+	testDataPathResult := doesDirectoryExist(z.TestDataPath)
+	_, executableErr := os.Stat(fmt.Sprintf("%z/cp-config", uuid))
+	_, configFileErr := os.Stat(fmt.Sprintf("%z/config/config.yaml", uuid))
+	_, logConfigFileErr := os.Stat(fmt.Sprintf("%z/config/log4rs.yaml", uuid))
+	_, anotherFileErr := os.Stat(fmt.Sprintf("%z/config/subfolder/another.yaml", uuid))
 	os.RemoveAll(uuid)
-	assert.True(s.T(), result.IsOk())
-	assert.True(s.T(), testDataPathResult)
-	assert.Equal(s.T(), nil, executableErr, "Expected executable file does not exist.")
-	assert.Equal(s.T(), nil, configFileErr, "Expected configuration file does not exist.")
-	assert.Equal(s.T(), nil, logConfigFileErr, "Expected log configuration file does not exist.")
-	assert.Equal(s.T(), nil, anotherFileErr, "Expected another file does not exist.")
+	assert.True(z.T(), result.IsOk())
+	assert.True(z.T(), testDataPathResult)
+	assert.Equal(z.T(), nil, executableErr, "Expected executable file does not exist.")
+	assert.Equal(z.T(), nil, configFileErr, "Expected configuration file does not exist.")
+	assert.Equal(z.T(), nil, logConfigFileErr, "Expected log configuration file does not exist.")
+	assert.Equal(z.T(), nil, anotherFileErr, "Expected another file does not exist.")
 }
 
 func TestZipExtractorTestSuite(t *testing.T) {
