91f29fe4a95a5ecd7e2e529e0b1602e9f3d5d63c
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2024-01-10 21:48:46 +0100

    + configuration provider

---
+++ pkg/core/config/file_provider_test.go
@@ -0,0 +1,62 @@
+package config
+
+import (
+	"github.com/simpleg-eu/cuplan-core/pkg/core"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+	"runtime"
+	"testing"
+	"time"
+)
+
+type FileProviderTestSuite struct {
+	suite.Suite
+	provider          *FileProvider
+	configurationFile string
+}
+
+func (f *FileProviderTestSuite) SetupTest() {
+	_, testFile, _, _ := runtime.Caller(0)
+	testDataPath := core.GetTestDataPath(testFile).Unwrap()
+	f.provider = NewFileProvider(testDataPath, core.NewCache(time.Hour), time.Hour)
+	f.configurationFile = "application.yaml"
+}
+
+func (f *FileProviderTestSuite) TestFileProvider_Get_ReturnsExpectedValue() {
+	const key = "Example:Inner:Value"
+	const value = 5
+
+	result := f.provider.Get(f.configurationFile, key)
+
+	assert.True(f.T(), result.IsOk())
+	assert.Equal(f.T(), value, result.Unwrap())
+}
+
+func (f *FileProviderTestSuite) TestFileProvider_Get_RootKey_ReturnsExpectedValue() {
+	const key = "Root"
+	const value = "yes"
+
+	result := f.provider.Get(f.configurationFile, key)
+
+	assert.True(f.T(), result.IsOk())
+	assert.Equal(f.T(), value, result.Unwrap())
+}
+
+func (f *FileProviderTestSuite) TestFileProvider_Get_EvenLevelKey_ReturnsExpectedValue() {
+	const key = "Example:Yeah"
+	const value = true
+
+	result := f.provider.Get(f.configurationFile, key)
+
+	assert.True(f.T(), result.IsOk())
+	assert.Equal(f.T(), value, result.Unwrap())
+}
+
+func (f *FileProviderTestSuite) TestFileProvider_Get_CachedKey_ReturnsExpectedValue() {
+	f.TestFileProvider_Get_ReturnsExpectedValue()
+	f.TestFileProvider_Get_ReturnsExpectedValue()
+}
+
+func TestFileProviderTestSuite(t *testing.T) {
+	suite.Run(t, new(FileProviderTestSuite))
+}
