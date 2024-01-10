277c66be796124551bfd21aec3123197e4e2f76a
Gabriel Amihalachioaie <gabriel@simpleg.eu>
2024-01-10 19:26:04 +0100

    + optional value

---
+++ pkg/core/option_test.go
@@ -0,0 +1,36 @@
+package core
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
+	"testing"
+)
+
+type OptionTestSuite struct {
+	suite.Suite
+}
+
+func (o *OptionTestSuite) TestOption_IsSome_ReturnsFalseForNone() {
+	assert.False(o.T(), None.IsSome())
+}
+
+func (o *OptionTestSuite) TestOption_IsNone_ReturnsTrueForNone() {
+	assert.True(o.T(), None.IsNone())
+}
+
+func (o *OptionTestSuite) TestOption_Unwrap_PanicsIfNone() {
+	assert.Panics(o.T(), func() { None.Unwrap() })
+}
+
+func (o *OptionTestSuite) TestOption_Unwrap_ReturnsValueIfSome() {
+	const value = "theValue:)"
+	option := Some(value)
+
+	unwrappedValue := option.Unwrap()
+
+	assert.Equal(o.T(), value, unwrappedValue)
+}
+
+func TestOptionTestSuite(t *testing.T) {
+	suite.Run(t, new(OptionTestSuite))
+}
